ca99adf6db98d7ba63a996fb4488b1afb9c741e9
Michael Ivashko <official.michael.developer@gmail.com>
2026-02-28 15:55:17 +0200

    configured mui with styled-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+
+const Btn = styled(Button)`
+  position: relative;
+`
+
 const Home = () => {
-  return <>fresh clean project</>
+  return (
+    <>
+      fresh clean project <Btn variant='contained'>Contained</Btn>
+    </>
+  )
 }
 
 export default Home

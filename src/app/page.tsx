9d7d70256fd6ceaf156e175f8479735abb1910df
Michael Ivashko <official.michael.developer@gmail.com>
2026-02-28 17:04:31 +0200

    first section bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
 'use client'
 
-import Button from '@mui/material/Button'
+import { Box } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-const Btn = styled(Button)`
+const FirstSection = styled(Box)`
   position: relative;
+  min-height: 100vh;
+  background: url('/bg.jpg') center top / cover no-repeat;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 362px;
+    background: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 1) 70px,
+      transparent 100%
+    );
+    pointer-events: none;
+    opacity: 0.7;
+  }
+`
+
+const SecondSection = styled(Box)`
+  min-height: 100vh;
 `
 
 const Home = () => {
   return (
     <>
-      fresh clean project <Btn variant='contained'>Contained</Btn>
+      <FirstSection></FirstSection>
+      <SecondSection></SecondSection>
     </>
   )
 }
